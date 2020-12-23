dd517f7939403908c09562f81e7a7300b52b877b
Johannes Wache <jbwa@posteo.de>
2020-12-23 19:01:35 +0100

    rewrote calc application

--- wasp/apps/fibonacci_clock.py
+++ wasp/apps/fibonacci_clock.py
@@ -4,23 +4,29 @@
 import wasp
 import icons
 
-MONTH = 'JanFebMarAprMaiJunJulAugSepOktNovDec'
+MONTH = "JanFebMarAprMaiJunJulAugSepOktNovDec"
 
-class FibonacciClockApp():
-    """Displays the time as a Fibonacci Clock
-    """
-    NAME = 'Fibo'
+
+class FibonacciClockApp:
+    """Displays the time as a Fibonacci Clock"""
+
+    NAME = "Fibo"
     ICON = icons.app
 
     def __init__(self):
         self.meter = wasp.widgets.BatteryMeter()
-        self.fields = [5,3,2,1,1]
-        self.field_colors = [0,0,0,0,0]
-        self.color_codes = [0xffff,0xf800,0x07e0,0x001f] # White, red, green and blue
+        self.fields = [5, 3, 2, 1, 1]
+        self.field_colors = [0, 0, 0, 0, 0]
+        self.color_codes = [
+            0xFFFF,
+            0xF800,
+            0x07E0,
+            0x001F,
+        ]  # White, red, green and blue
 
     def foreground(self):
         """Activate the application."""
-        self.on_screen = ( -1, -1, -1, -1, -1, -1 )
+        self.on_screen = (-1, -1, -1, -1, -1, -1)
         self.draw()
         wasp.system.request_tick(1000)
 
@@ -32,27 +38,26 @@ class FibonacciClockApp():
 
     def tick(self, ticks):
         self.update()
-    
-    def calculateColors(self,hr,mn):
-        self.field_colors = [0,0,0,0,0]
-        if (hr >= 12):
+
+    def calculateColors(self, hr, mn):
+        self.field_colors = [0, 0, 0, 0, 0]
+        if hr >= 12:
             hr -= 12
         for i in range(5):
-            if ((hr - self.fields[i]) >= 0):
+            if (hr - self.fields[i]) >= 0:
                 hr -= self.fields[i]
                 self.field_colors[i] += 1
-            
-            if ((mn - self.fields[i]) >= 0):
+
+            if (mn - self.fields[i]) >= 0:
                 mn -= self.fields[i]
                 self.field_colors[i] += 2
-        
 
     def draw(self):
         """Redraw the display from scratch."""
         draw = wasp.watch.drawable
 
         draw.fill()
-        self.on_screen = ( -1, -1, -1, -1, -1, -1 )
+        self.on_screen = (-1, -1, -1, -1, -1, -1)
         self.update()
         self.meter.draw()
 
@@ -70,21 +75,32 @@ class FibonacciClockApp():
             return False
         draw = wasp.watch.drawable
 
-        #calculate colors of fields:
-        self.calculateColors(now[3],now[4] // 5) # Clock can only display every 5 minutes
+        # calculate colors of fields:
+        self.calculateColors(
+            now[3], now[4] // 5
+        )  # Clock can only display every 5 minutes
 
-        draw.fill(x=71,y=41,w=23,h=23,bg=self.color_codes[self.field_colors[4]]) # 1 field
-        draw.fill(x=71,y=66,w=23,h=23,bg=self.color_codes[self.field_colors[3]]) # 1 field
-        draw.fill(x=21,y=41,w=48,h=48,bg=self.color_codes[self.field_colors[2]]) # 2 field
-        draw.fill(x=21,y=91,w=73,h=73,bg=self.color_codes[self.field_colors[1]]) # 3 field
-        draw.fill(x=96,y=41,w=123,h=123,bg=self.color_codes[self.field_colors[0]]) # 5 field
+        draw.fill(
+            x=71, y=41, w=23, h=23, bg=self.color_codes[self.field_colors[4]]
+        )  # 1 field
+        draw.fill(
+            x=71, y=66, w=23, h=23, bg=self.color_codes[self.field_colors[3]]
+        )  # 1 field
+        draw.fill(
+            x=21, y=41, w=48, h=48, bg=self.color_codes[self.field_colors[2]]
+        )  # 2 field
+        draw.fill(
+            x=21, y=91, w=73, h=73, bg=self.color_codes[self.field_colors[1]]
+        )  # 3 field
+        draw.fill(
+            x=96, y=41, w=123, h=123, bg=self.color_codes[self.field_colors[0]]
+        )  # 5 field
 
         self.on_screen = now
 
         month = now[1] - 1
-        month = MONTH[month*3:(month+1)*3]
-        draw.string('{}. {} {}'.format(now[2], month, now[0]),
-                0, 185, width=240)
+        month = MONTH[month * 3 : (month + 1) * 3]
+        draw.string("{}. {} {}".format(now[2], month, now[0]), 0, 185, width=240)
 
         self.meter.update()
         return True
